506b36130f212ac29c68fcf865fe34d60a88ddce
Rain Zimin Yang <rainyang2005@163.com>
2025-12-08 11:56:16 -0800

    Import UBC CS personal website

---
+++ mathjax-config.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        macros: {
+            rdeg: "{\\mathrm{rdeg}}",
+            ndeg: "{\\mathrm{ndeg}}",
+            AND: "{\\mathrm{AND}}",
+            OR: "{\\mathrm{OR}}",
+            supp: "{\\mathrm{Supp}}",
+            RR: "{\\mathbb{R}}",
+            NN: "{\\mathbb{N}}",
+            PARITY: "{\\mathrm{PARITY}}"
+        }
+    },
+    svg: {
+        fontCache: 'global'
+    }
+};
