3eeb14cb85e426c6f33a0d7e734e8631db9e37ab
Rain Zimin Yang <rainziminyang@gmail.com>
2026-01-13 19:19:14 -0800

    updated CV, added more research

--- mathjax-config.js
+++ mathjax-config.js
@@ -7,7 +7,11 @@ window.MathJax = {
             AND: "{\\mathrm{AND}}",
             OR: "{\\mathrm{OR}}",
             supp: "{\\mathrm{Supp}}",
+            AA: "{\\mathbb{A}}",
+            PP: "{\\mathbb{P}}",
             RR: "{\\mathbb{R}}",
+            CC: "{\\mathbb{C}}",
+            QQ: "{\\mathbb{Q}}",
             NN: "{\\mathbb{N}}",
             FF: "{\\mathbb{F}}",
             PARITY: "{\\mathrm{PARITY}}"
